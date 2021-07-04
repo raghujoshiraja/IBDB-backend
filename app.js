148c0fcf0c0d5a0937d9d2bdb4a7900351d58f5a
Raghu Raja <r.raja3@wlv.ac.uk>
2021-07-04 23:57:55 +0530

    added example env and cors to the app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 // PORT
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 4020;
 // Initialization
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // MongoDB ORM connection
 mongoose
